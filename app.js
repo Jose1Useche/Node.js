757d3674135e6a646fdb39f046202e26a71c2c88
Jose Useche <jose1useche@gmail.com>
2023-04-22 19:27:30 -0400

    fs.appendFileSync

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 import * as fs from 'node:fs';
 
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!');
+// fs.writeFileSync('notes.txt', 'This file was created by Node.js!');
+
+fs.appendFileSync('notes.txt', '\nInsertando una nueva línea sin borrar la anterior en el .txt');
