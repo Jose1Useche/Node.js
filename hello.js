21eb3711477188af484388e9b9caa95cc57806b3
Jose Useche <jose1useche@gmail.com>
2023-04-22 20:14:19 -0400

    import .js into .js

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
-console.log('Welcome to the class!');
+import * as m from './message.js';
+
+console.log('Welcome to the class: ', m.name, m.lastName);
+
+const sumatoria = m.sumatoria(5,5);
+console.log(sumatoria);
---
+++ message.js
@@ -0,0 +1,7 @@
+console.log('Hola desde otro mensaje!!!');
+
+export const name = 'Jose de Jesus';
+export const lastName = 'Useche Cuevas';
+export const sumatoria = function (a,b) {
+    return a + b;
+}
