81acd1fec6740a5404fe781e3d9de16a0a2606fc
Jose Useche <jose1useche@gmail.com>
2023-04-26 19:58:55 -0400

    Getting Inputs: Inicio

---
+++ getting-inputs.js
@@ -0,0 +1,10 @@
+const entrada = process.argv[2];
+
+// console.log(process.argv);
+// console.log(entrada);
+
+if (entrada === 'Jose') {
+    console.log('Hola soy ' + entrada);
+} else {
+    console.log('No eres ' + entrada);
+}
