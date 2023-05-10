e97cc368a0627f54617fcc10f16fcd68cec093fe
Jose Useche <jose1useche@gmail.com>
2023-05-09 21:12:01 -0400

    Concatenacion de requests, destructuracion de JS Arrays y un poco de Callback Functions

--- weather-app/app2.js
+++ weather-app/app2.js
@@ -4,14 +4,14 @@ let latitude;
 let longitude;
 let site = 'Santa Teresa Del Tuy';
 
-axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${site}.json?access_token=pk.eyJ1Ijoiam9zZTF1c2VjaGUiLCJhIjoiY2xoOTVtOWhoMDQ1azNob2FzdXk5MXp4dSJ9.uJWmjljssJceIJ5owOKbYA&limit=1`)
+axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(site)}.json?access_token=pk.eyJ1Ijoiam9zZTF1c2VjaGUiLCJhIjoiY2xoOTVtOWhoMDQ1azNob2FzdXk5MXp4dSJ9.uJWmjljssJceIJ5owOKbYA&limit=1`)
 .then(response => {
     // handle success
     /*The coordinates of the feature’s center in the form [longitude,latitude]. This may be the literal centroid of the feature’s geometry, or the center of 
     human activity within the feature (for example, the downtown area of a city).*/
-    latitude = response.data.features[0].center[1];
-    longitude = response.data.features[0].center[0];
-    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+    [longitude, latitude] = response.data.features[0].center;
+    //  = response.data.features[0].center[0];
+    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`, encodeURIComponent(site));
 
     return axios.get(`http://api.weatherstack.com/current?access_key=f72acb9cb0f9cd7da23f7e37e5fbe9b2&query=${latitude},${longitude}&units=m`)
 })
