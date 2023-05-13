81b498413c3c94241ea8553910265692cfe00730
Jose Useche <jose1useche@gmail.com>
2023-05-13 17:47:40 -0400

    Express: envío de tags, JSON y JSON arrays.

--- express/web-server.js
+++ express/web-server.js
@@ -2,21 +2,39 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello World');
 });
 
 app.get('/help', (req, res) => {
-  res.send('Help page')
+  res.send(`<h1>
+                Help page
+            </h1>`
+          );
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page')
+  res.send(
+    {
+      name: 'Jose',
+      lastName: 'Useche'
+    }
+  );
 });
 
 app.get('/weather', (req, res) => {
-  res.send('Weather page')
+  res.send([
+    {
+      name: 'Nohemi'
+    },
+    {
+      name: 'Maria'
+    },
+    {
+      name: 'Juan'
+    }
+  ]);
 });
 
 app.listen(3000,() => {
-    console.log('Server is up on port 3000.')
+    console.log('Server is up on port 3000.');
 });
