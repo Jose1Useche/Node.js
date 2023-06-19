6db7eeb6189a4bfdd72c612d5f6f9f35a10a9d5f
Jose Useche <jose1useche@gmail.com>
2023-06-19 10:41:13 -0400

    Usando fetch (JS method) para consumir API's desde el front

--- express/public/js/app.js
+++ express/public/js/app.js
@@ -1,1 +1,7 @@
 console.log('Client side javascript is loaded!');
+
+fetch('http://localhost:3000/weather?address=Santa%20Teresa%20Del%20Tuy').then(response => {
+    console.log(response);
+    
+    response.json().then(data => console.log(data));
+});
