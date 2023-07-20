cbcd900c7cb150de93c9bba3a283b1f97b106b0c
Jose Useche <jose1useche@gmail.com>
2023-07-19 20:00:47 -0400

    Mongodb: deleteOne() y updateOne()

--- express/web-server.js
+++ express/web-server.js
@@ -173,6 +173,39 @@ app.post('/books', (req, res) => {
       res.status(500).json({ error: 'Could not create a new document'});
     })
 });
+
+app.delete('/books/:id', (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection('books')
+      .deleteOne({ _id: new ObjectId(req.params.id) })
+      .then(result => {
+        res.status(200).json(result);
+      })
+      .catch(() => {
+        res.status(500).json({ error: 'Could not delete the document.' });
+      });
+  } else {
+    res.status(500).json({ error: 'Not a valid doc id'})
+  }
+});
+
+app.patch('/books/:id', (req, res) => {
+  const updates = req.body;
+  console.log(req.body);
+
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection('books')
+      .updateOne({ _id: new ObjectId(req.params.id) }, { $set: updates })
+      .then(result => {
+        res.status(200).json(result);
+      })
+      .catch(() => {
+        res.status(500).json({ error: 'Could not update the document.' });
+      });
+  } else {
+    res.status(500).json({ error: 'Not a valid doc id'});
+  }
+});
 /********************************************************************/
 /*****************************Mongodb END****************************/
 /********************************************************************/
