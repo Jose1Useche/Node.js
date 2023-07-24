d8c2af7ef1b64eec2e4e3026b273144096bb3883
Jose Useche <jose1useche@gmail.com>
2023-07-24 19:51:47 -0400

    MongoDB Atlas

--- mongodb/mdb.js
+++ mongodb/mdb.js
@@ -1,9 +1,10 @@
 import { MongoClient } from "mongodb";
 
 let dbConnection;
+let uri = 'mongodb+srv://jose1useche:sVEjL5FtjthryxZY@cluster0.no7f8lk.mongodb.net/?retryWrites=true&w=majority';
 
 export const connectToDb = cbf => {
-    MongoClient.connect('mongodb://127.0.0.1:27017/bookstore')
+    MongoClient.connect(uri) //('mongodb://127.0.0.1:27017/bookstore')
             .then(client => {
                 dbConnection = client.db();
                 return cbf();
