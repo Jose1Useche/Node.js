0eaa67751c2ed3afbc4b12603752ea1438f98ecb
Jose Useche <jose1useche@gmail.com>
2023-08-18 15:22:14 -0400

    Logout...

--- express/middlewares/auth.js
+++ express/middlewares/auth.js
@@ -5,15 +5,17 @@ const jwt = jsonwebtoken;
 
 export const auth =  async (req, res, next) => {
     try {
-        const token = req.header('Autorizacion').replace('Bearer ', '');
+        const token = req.header('Authorization').replace('Bearer ', '');
         const decoded = jwt.verify(token, 'palabraClaveSecreta');
-        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token }, {password: 0, tokens: 0});
+        // const user = await User.findOne({ _id: decoded._id, 'tokens.token': token }, {password: 0, tokens: 0});
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token }, {password: 0});
 
         if (!user) {
             throw new Error();
         }
 
         req.user = user;
+        req.token = token;
         next();
     } catch (error) {
         res.status(401).send({ error: 'Please authenticate.' });
--- express/routers/user.js
+++ express/routers/user.js
@@ -73,3 +73,29 @@ userRouter.get('/users/me', auth, async (req, res) => {
   //   res.status(500).send(error.message)    ;
   // }
 });
+
+//logout
+userRouter.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(token => {
+      return token.token !== req.token
+    });
+    await req.user.save();
+
+    res.send();
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+//logout All
+userRouter.post('/users/logoutall', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+
+    res.send();
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
