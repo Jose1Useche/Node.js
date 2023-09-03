0532e35afa3afb99e7fbdf9888007f4a19930c33
Jose Useche <jose1useche@gmail.com>
2023-09-03 17:28:18 -0400

    Multer: Validando carga de archivos (capacidad y tipos de archivos)

--- express/routers/multer.js
+++ express/routers/multer.js
@@ -4,7 +4,21 @@ import express from "express";
 export const multerRouter = new express.Router();
 
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        // if(!file.originalname.endsWith('.pdf')) {
+        //     return cb(new Error('Please upload a PDF'));
+        // }
+
+        if(!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word document'));
+        }
+
+        cb(undefined, true);
+    }
 });
 
 multerRouter.post('/upload', upload.single('upload'), (req, res) => {
